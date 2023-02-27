32e7f261362c37f569bd238ac29e73e0e4a50bc6
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-02-26 22:16:31 -0300

    version 1.0

--- src/main/java/com/aps/schoolife/fachada/Fachada.java
+++ src/main/java/com/aps/schoolife/fachada/Fachada.java
@@ -1,11 +1,29 @@
 package com.aps.schoolife.fachada;
 
+import com.aps.schoolife.controllers.*;
+import com.aps.schoolife.models.Disciplina;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @Scope("singleton")
 public class Fachada {
+
+    @Autowired
+    private AlunoController alunoController;
+
+    @Autowired
+    private DisciplinaController disciplinaController;
+
+    @Autowired
+    private NotaController notaController;
+
+    @Autowired
+    private ProfessorController professorController;
+
+    @Autowired
+    private TurmaController turmaController;
 
 
 }
