bfad681a6ed6b3c717a893f5d08177b16a88b932
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-03-19 01:10:55 -0300

    version 1.0 - Login Google

--- src/main/java/com/aps/schoolife/SecurityConfig.java
+++ src/main/java/com/aps/schoolife/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.aps.schoolife;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        RequestMatcher loginAndErrorRequestMatcher = request ->
+                request.getRequestURI().startsWith("/login") || request.getRequestURI().startsWith("/error");
+
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(loginAndErrorRequestMatcher).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
 }
