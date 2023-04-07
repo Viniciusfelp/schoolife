3178443d5be964a61dff42667476fa9ad49ac645
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-04-07 14:28:38 -0300

    version 1.0 - add news module

--- class-service/src/main/java/com/aps/classs/ClassServiceApplication.java
+++ class-service/src/main/java/com/aps/classs/ClassServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClassServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClassServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClassServiceApplication.class, args);
+    }
 
 }
