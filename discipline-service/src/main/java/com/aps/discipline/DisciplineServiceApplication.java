101418b06c6e3fa48415ee90a97915681abc020f
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-04-07 14:28:38 -0300

    version 1.0 - Reformulação

--- discipline-service/src/main/java/com/aps/discipline/DisciplineServiceApplication.java
+++ discipline-service/src/main/java/com/aps/discipline/DisciplineServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DisciplineServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DisciplineServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DisciplineServiceApplication.class, args);
+    }
 
 }
