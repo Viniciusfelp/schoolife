ed14fb66492d0cfa2ac1e6b24ab0b851103364dd
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-04-17 05:19:59 -0300

    Versão 1.0 - Atualizações

--- notaFrequencia-service/src/main/java/br/ufpe/cin/aps/notafrequenciaservice/security/SecurityConfig.java
+++ notaFrequencia-service/src/main/java/br/ufpe/cin/aps/notafrequenciaservice/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Autowired
